46eced69134d4a6d974f864545d42de38e8916b0
Giovanna <giovannad1clonius@gmail.com>
2023-01-07 01:34:10 -0300

    JavaScript com o bonus incompleto

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let prato;
+
 function selecionarPrato(pratoSelecionado){
 
     const clearSelecionado = document.querySelector('.pratos .selecionado');
@@ -7,7 +9,15 @@ function selecionarPrato(pratoSelecionado){
     } 
 
     pratoSelecionado.classList.add('selecionado');
+
+    let nomePrato = document.querySelector('.pratos .selecionado');
+    prato = nomePrato.querySelector('.nome-item').innerHTML
+    console.log(prato)
+
+    verificaPedido();
 }
+
+let bebida; 
 
 function selecionarBebida(bebidaSelecionada){
 
@@ -18,7 +28,15 @@ function selecionarBebida(bebidaSelecionada){
     }
 
     bebidaSelecionada.classList.add('selecionado');
+
+    let nomeBebida = document.querySelector('.bebidas .selecionado');
+    bebida = nomeBebida.querySelector('.nome-item').innerHTML
+    console.log(bebida)
+
+    verificaPedido();
 }
+
+let sobremesa;
 
 function selecionarSobremesa(sobremesaSelecionada){
 
@@ -29,6 +47,33 @@ function selecionarSobremesa(sobremesaSelecionada){
     }
 
     sobremesaSelecionada.classList.add('selecionado');
+
+    let nomeSobremesa = document.querySelector('.sobremesas .selecionado');
+    sobremesa = nomeSobremesa.querySelector('.nome-item').innerHTML
+    console.log(sobremesa)
+
+    verificaPedido();
 }
 
+function verificaPedido(){
+
+    if (prato !== undefined && bebida !== undefined && sobremesa !== undefined) {
+ 
+        const botao = document.querySelector(".botao");
+        botao.classList.add('finalizado')
+        botao.innerHTML = "Fechar pedido";
+        
+}
+
+}
+
+function finalizarPedido(){
+    if (prato !== undefined && bebida !== undefined && sobremesa !== undefined) {
+    document.querySelector('.finalizar-pedido').classList.remove('escondido');
+    }
+}
+
+function cancelarPedido(){
+    document.querySelector('.finalizar-pedido').classList.add('escondido');
+}
 
